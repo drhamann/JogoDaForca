8fdc0d24a5e228cdb81f8e25dad7f34b68be10cd
George Hamann <george.hamann@gmail.com>
2024-05-17 08:46:15 -0300

    Jogo da forca refatorado

--- JogoDaForca/JogoDaForca.cs
+++ JogoDaForca/JogoDaForca.cs
@@ -2,96 +2,104 @@
 {
     public class JogoDaForca
     {
-        public string[] palavras { get; set; }
-        char[] acertos { get; set; }
-        List<string> letrasUsada { get; set; } = new List<string>();
-        public string palavraParaAdivinhar { get; set; }
-        public int erros { get; set; } = 0;
+        public static string[] Palavras { get; set; }
+        private char[] Acertos { get; set; }
+        private List<string> LetrasUsada { get; set; }
+        private string PalavraParaAdivinhar { get; set; }
+        private int Erros { get; set; }
+        private int Tentativas
+        {
+            get
+            {
+                return LetrasUsada.Count;
+            }
+        }
+        private string Tentativa { get; set; }
 
         public JogoDaForca()
         {
-            palavras =
+            Palavras =
                 [
                 "ABACATE", "ABACAXI", "ACEROLA", "AÇAÍ", "ARAÇA", "BACABA", "BACURI",
                 "BANANA", "CAJÁ", "CAJÚ", "CARAMBOLA", "CUPUAÇU", "GRAVIOLA",
                 "GOIABA", "JABUTICABA", "JENIPAPO", "MAÇÃ", "MANGABA", "MANGA",
                 "MARACUJÁ", "MURICI", "PEQUI", "PITANGA", "PITAYA", "SAPOTI",
                 "TANGERINA", "UMBU", "UVA", "UVAIA"
-        ];
-
-
-            Selecionar();
-            acertos = new char[palavraParaAdivinhar.Length];
-
-        }
-
-
-        private void Selecionar()
-        {
+                ];
+            LetrasUsada = new List<string>();
             Random random = new Random();
-            palavraParaAdivinhar = palavras[Convert.ToInt32(random.Next(palavras.Length))];
+            PalavraParaAdivinhar = Palavras[Convert.ToInt32(random.Next(Palavras.Length))];
+            Erros = 1;
+            Acertos = new char[PalavraParaAdivinhar.Length];
+            for (int i = 0; i < Acertos.Length; i++)
+            {
+                Acertos[i] = '_';
+            }
         }
 
         public void Responder()
         {
-            string resposta = " ";
-
             do
             {
+                DesenharForca();
 
-                Console.WriteLine("Escolha uma letra A...Z: ");
-                resposta = Console.ReadLine().ToLower();
-                letrasUsada.Add(resposta);
-
-                Console.WriteLine("Letras usadas: ");
-                foreach (string letra in letrasUsada)
+                if (EscolherLetra() is false)
                 {
-                    Console.Write(letra);
+                    continue;
                 }
 
-                Console.WriteLine("\n ");
-
-                bool acertouPalavra = false;
-                for (int i = 0; i < palavraParaAdivinhar.Length; i++)
-                {
-                    if (resposta[0] == palavraParaAdivinhar[i])
-                    {
-                        acertos[i] = palavraParaAdivinhar[i];
-                        acertouPalavra = true;
-                    }
-                }
-                if (acertouPalavra is false)
-                {
-                    erros++;
-                }
-
-                foreach (char letra in acertos)
-                {
-                    Console.WriteLine("certa resposta, a palavra era: " + letra);
-                }
+                VerificarTentativa();
             }
-            while (this.AcertouPalavraCheia() is false && erros < 5);
+            while (DesenharForca());
         }
 
-        private bool AcertouPalavraCheia()
+        private bool EscolherLetra()
         {
-            for (int i = 0; i < palavraParaAdivinhar.Length; i++)
+            Console.WriteLine("Escolha uma letra A...Z: ");
+            Tentativa = Console.ReadLine().ToUpper();
+            if (string.IsNullOrEmpty(Tentativa) ||
+                int.TryParse(Tentativa, out int numero)
+                || Tentativa.Length > 1)
             {
-                if (acertos[i] != palavraParaAdivinhar[i])
-                {
-                    return false;
-                }
+                Console.WriteLine($"Insira uma letra");
+                Console.ReadKey();
+                return false;
             }
+            if (LetrasUsada.Contains(Tentativa))
+            {
+                Console.WriteLine($"Letra já usada '{Tentativa}', tenta usar outra.");
+                Console.ReadKey();
+                return false;
+            }
+            LetrasUsada.Add(Tentativa);
             return true;
         }
 
-        // Método para exibir o desenho do boneco baseado no número de erros
-        private void ExibirDesenhoBoneco(int erros)
+        private void VerificarTentativa()
         {
-            string cabeca = erros > 0 ? "(oo)" : "    ";
-            string bracos = erros > 1 ? "// \\" : "    ";
-            string tronco = erros > 2 ? "  |" : "   ";
-            string pernas = erros > 3 ? "// \\" : "    ";
+            bool acertouPalavra = false;
+            for (int i = 0; i < PalavraParaAdivinhar.Length; i++)
+            {
+                if (Tentativa[0] == PalavraParaAdivinhar[i].ToString().ToUpper()[0])
+                {
+                    Acertos[i] = Tentativa[0];
+                    acertouPalavra = true;
+                }
+            }
+            if (acertouPalavra is false)
+            {
+                Erros++;
+            }
+        }
+
+        // Método para exibir o desenho do boneco baseado no número de erros
+        private bool DesenharForca()
+        {
+            Console.Clear();
+            string cabeca = Erros > 0 ? @" (oo) " : "    ";
+            string bracos = Erros > 1 ? @"//  \\" : "    ";
+            string tronco = Erros > 2 ? @"  || " : "    ";
+            string pernas = Erros > 3 ? @"//  \\" : "    ";
 
             string desenho = $@"
 =========
@@ -101,9 +109,51 @@
 !       {tronco}
 !       {pernas}
 !
-=========";
+=========
+Número de tentativas: {Tentativas}
+Palavras escolhidas : {LetrasUsadas()}
+Palavra: {MontarAcertos()}
+";
 
             Console.WriteLine(desenho);
+            if (Erros > 4)
+            {
+                Console.WriteLine("###Você perdeu!!");
+                return true;
+            }
+            return AcertouPalavraCheia() is false;
+        }
+
+        private string LetrasUsadas()
+        {
+            string letras = string.Empty;
+            foreach (string letra in LetrasUsada)
+            {
+                letras += letra;
+            }
+            return letras;
+        }
+
+        private string MontarAcertos()
+        {
+            string acerto = string.Empty;
+            foreach (char letra in this.Acertos)
+            {
+                acerto += letra;
+            }
+            return acerto;
+        }
+        private bool AcertouPalavraCheia()
+        {
+            for (int i = 0; i < PalavraParaAdivinhar.Length; i++)
+            {
+                if (Acertos[i] != PalavraParaAdivinhar[i])
+                {
+                    return false;
+                }
+            }
+            Console.WriteLine("###Você acertou!!");
+            return true;
         }
     }
 }
